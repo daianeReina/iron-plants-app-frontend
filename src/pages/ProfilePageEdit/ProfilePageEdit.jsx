8e4eb1f8864d47e69b2dd7ad95afca048bd153d2
Daiane Reina <daiane.veras.reina@gmail.com>
2022-10-22 10:06:56 +0100

    FEAT: Redirection to the user home, page user settings

--- src/pages/ProfilePageEdit/ProfilePageEdit.jsx
+++ src/pages/ProfilePageEdit/ProfilePageEdit.jsx
@@ -1,8 +1,10 @@
 import "./ProfilePageEdit.css";
-
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
 
 function ProfilePageEdit() {
+  const { isLoggedIn, user } = useContext(AuthContext);
   return (
     <div>
       <h1> Edit you settings</h1>
