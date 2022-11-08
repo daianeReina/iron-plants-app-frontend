7759707e1cdf069add9ddec8c632d47f7dc91112
Daiane Reina <daiane.veras.reina@gmail.com>
2022-11-08 12:10:36 +0000

    style Login Page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -45,42 +45,101 @@ function LoginPage() {
   };
 
   return (
-    <div className="LoginPage container">
-      <h1 className="h3 mb-3 fw-normal">Please Sign In</h1>
-
-      <form onSubmit={handleLoginSubmit}>
-        <div className="form-floating">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="name@example.com"
-            name="email"
-            value={email}
-            onChange={handleEmail}
-          />
-          <label for="floatingInput">Email Address</label>
+    <>
+      <section className="Form my-4 mx-5">
+        <div className="container">
+          <div className="row ">
+            <div className="col">
+              <img
+                src="/plant-horizontal-2.png"
+                className="imgLogin img-fluid"
+                alt="loginImage"
+              />
+            </div>
+            <div className="col px-5 pt-5">
+              <h1 className="text-start font-weith-bold py-3">Log In</h1>
+              <h4 className="text-start"> Sign into your account</h4>
+              <form onSubmit={handleLoginSubmit}>
+                <div className="form-row">
+                  <div className="col">
+                    <input
+                      type="email"
+                      className="form-control my-3"
+                      placeholder="Your email"
+                      name="email"
+                      value={email}
+                      onChange={handleEmail}
+                    />
+                  </div>
+                  <div className="col">
+                    <input
+                      type="password"
+                      className="form-control my-3 "
+                      placeholder="Your password"
+                      name="password"
+                      value={password}
+                      onChange={handlePassword}
+                    />
+                  </div>
+                  <div className="col">
+                    <button
+                      className="w-100 btn btn-outline-success me-2"
+                      type="submit"
+                    >
+                      Login
+                    </button>
+                  </div>
+                  {errorMessage && (
+                    <p className="error-message">{errorMessage}</p>
+                  )}
+                  <p className="text-start my-3">
+                    Don't have an account?
+                    <Link to={"/signup"}>Register Here</Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="form-floating">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="password"
-            name="password"
-            value={password}
-            onChange={handlePassword}
-          />
-          <label>Password:</label>
-        </div>
+      </section>
+    </>
 
-        <button className="w-100 btn btn-lg btn-success" type="submit">
-          Login
-        </button>
-      </form>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+    // <div className="LoginPage container">
+    //   <h1 className="h3 mb-3 fw-normal">Please Sign In</h1>
 
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
-    </div>
+    //   <form onSubmit={handleLoginSubmit}>
+    //     <div className="form-floating">
+    //       <input
+    //         type="email"
+    //         className="form-control"
+    //         placeholder="name@example.com"
+    //         name="email"
+    //         value={email}
+    //         onChange={handleEmail}
+    //       />
+    //       <label for="floatingInput">Email Address</label>
+    //     </div>
+    //     <div className="form-floating">
+    //       <input
+    //         type="password"
+    //         className="form-control"
+    //         placeholder="password"
+    //         name="password"
+    //         value={password}
+    //         onChange={handlePassword}
+    //       />
+    //       <label>Password:</label>
+    //     </div>
+
+    //     <button className="w-100 btn btn-lg btn-success" type="submit">
+    //       Login
+    //     </button>
+    //   </form>
+    //   {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+    //   <p>Don't have an account yet?</p>
+    //   <Link to={"/signup"}> Sign Up</Link>
+    // </div>
   );
 }
 
